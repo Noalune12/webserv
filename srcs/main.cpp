8b92c1131f1de191b1ef472f87ec766eedfc4a15
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2025-10-09 08:26:23 +0200

    chore: comments for what main does

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -9,5 +9,14 @@ int	main(void) {
 
 	std::cout << (test._value ? test._value : 100) << std::endl;
 
+	/* main behavior:
+
+	"Your program must use a configuration file, provided as an argument on the command line, or available in a default path."
+
+	check if one arg	-> must be configuration file
+	if no arg			-> launch server with own config file.
+
+	*/
+
 	return (EXIT_SUCCESS);
 }
