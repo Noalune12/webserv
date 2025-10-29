b4f2c69761ae6299f4969fb42d5b03afa842e91a
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2025-10-29 18:18:11 +0100

    test: Called FileReader in Server constructor

--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -1,19 +1,32 @@
+#include <iostream>
+
+// temp
+#include "Config.hpp"
+
+#include "FileReader.hpp"
 #include "Server.hpp"
 
-#include <iostream>
 
 Server::Server() : _config() {}
 
 Server::Server(const std::string& configFile) {
 
-	try
-	{
+	try {
 		/* calls the Facade design pattern for the configuration file here ? */
 		// this->_config = this->_config.JE_SAIS_PAS_ENCORE();
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
+
+
+
+		// autre design en fonction de ce que j'ai fais pour FileReader.
+		FileReader reader(configFile);
+
+		std::string rawConfig = reader.getFileContent();
+
+		// A la place de la ligne au dessus on pourrait avoir
+		Config config(reader.getFileContent());
+
+	} catch(const std::exception& e) {
+        std::cerr << "Server initialization failed: " << e.what() << std::endl;
 	}
 }
 
