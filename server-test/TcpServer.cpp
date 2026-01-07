a3a05de125c2747c1fcc0305beef3b8c0a96e57b
Noalune12 <louanne.buisson@gmail.com>
2026-01-07 11:08:23 +0100

    test

--- server-test/TcpServer.cpp
+++ server-test/TcpServer.cpp
@@ -346,6 +346,8 @@ bool hasWS(std::string s) {
     return false;
 }
 
+//if body -> check content-length is same
+
 bool TcpServer::checkHeaders(std::string headers) {
     _headers.clear();
     std::stringstream ss(headers);
@@ -358,8 +360,8 @@ bool TcpServer::checkHeaders(std::string headers) {
         }
         line = line.substr(0, line.length() - 1);
         size_t index = line.find(": ");
-        std::cout << "index : = " << index << std::endl;
-        std::cout << "HEADER = " << line << std::endl;
+        // std::cout << "index : = " << index << std::endl;
+        // std::cout << "HEADER = " << line << std::endl;
         if (index == 0 || index == std::string::npos || index == line.length() - 2) {
             send400();
             std::cout << "error with headers no : or no header name or content" << std::endl;
@@ -388,16 +390,11 @@ bool TcpServer::checkHeaders(std::string headers) {
     for (; it != _headers.end(); it++) {
         std::cout << "[" << it->first << ", " << it->second << "]" << std::endl;
     }
-
+    // has host
     return true;
 }
 
 bool TcpServer::checkRequestLine(std::string& method, std::string& uri, std::string& http) {
-    // if (_req.empty()) {
-    //     send400();
-    //     std::cout << "error no headers" << std::endl;
-    //     return false;
-    // }
     if (method != "GET" && method != "POST" && method != "DELETE") { //what if method is not accepted
         send400();
         std::cout << "error with method" << std::endl;
