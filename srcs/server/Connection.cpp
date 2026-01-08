dc4d97bdd56ea61796afa2b76c1a86518570657a
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2026-01-08 11:05:09 +0100

    feat: adding data related to buffers to Connection

--- srcs/server/Connection.cpp
+++ srcs/server/Connection.cpp
@@ -1,11 +1,13 @@
 #include "Connection.hpp"
 
-Connection::Connection() : _clientFd(-1), _state(READING_REQUEST) {}
+Connection::Connection() : _clientFd(-1), _state(READING_REQUEST), _buffer(), _bufferLenght(-1) {}
 
-Connection::Connection(int clientFd) : _clientFd(clientFd), _state(READING_REQUEST) {}
+Connection::Connection(int clientFd) : _clientFd(clientFd), _state(READING_REQUEST), _buffer(), _bufferLenght(-1) {}
 
 Connection::~Connection() {
 	(void) _clientFd;
 	(void) _state;
+	(void) _buffer;
+	(void) _bufferLenght;
 	// /!\ do not close _clientFd, it should be managed by EventLopp
 }
