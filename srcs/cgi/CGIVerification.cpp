ebb70ff5c3277d2e7fe42d9b53eb46e8da184a33
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2026-02-07 11:55:15 +0100

    fix: updated error codes for cgi rights/existence issues

--- srcs/cgi/CGIVerification.cpp
+++ srcs/cgi/CGIVerification.cpp
@@ -7,10 +7,12 @@
 #include "CGIExecutor.hpp"
 #include "Logger.hpp"
 
-bool	CGIExecutor::verifyCGIPath(const std::string& path) {
+bool	CGIExecutor::verifyCGIPath(const std::string& path, Connection& client) {
 
 	if (access(path.c_str(), F_OK) != 0) {
 		Logger::error("CGI interpreter not found: " + path);
+		client._request.err = true;
+		client._request.status = 500;
 		return (false);
 	}
 
@@ -18,26 +20,34 @@ bool	CGIExecutor::verifyCGIPath(const std::string& path) {
 
 	if (stat(path.c_str(), &st) != 0) {
 		Logger::error("Cannot stat CGI interpreter: " + path);
+		client._request.err = true;
+		client._request.status = 500;
 		return (false);
 	}
 
 	if (!S_ISREG(st.st_mode)) {
 		Logger::error("CGI interpreter is not a regular file: " + path);
+		client._request.err = true;
+		client._request.status = 500;
 		return (false);
 	}
 
 	if (access(path.c_str(), X_OK) != 0) {
 		Logger::error("CGI interpreter is not executable: " + path);
+		client._request.err = true;
+		client._request.status = 500;
 		return (false);
 	}
 
 	return (true);
 }
 
-bool	CGIExecutor::verifyCGIScript(const std::string& scriptPath) {
+bool	CGIExecutor::verifyCGIScript(const std::string& scriptPath, Connection& client) {
 
 	if (access(scriptPath.c_str(), F_OK) != 0) {
 		Logger::error("CGI script not found: " + scriptPath);
+		client._request.err = true;
+		client._request.status = 404;
 		return (false);
 	}
 
@@ -45,16 +55,22 @@ bool	CGIExecutor::verifyCGIScript(const std::string& scriptPath) {
 
 	if (stat(scriptPath.c_str(), &st) != 0) {
 		Logger::error("Cannot stat CGI script: " + scriptPath);
+		client._request.err = true;
+		client._request.status = 404;
 		return (false);
 	}
 
 	if (!S_ISREG(st.st_mode)) {
 		Logger::error("CGI script is not a regular file: " + scriptPath);
+		client._request.err = true;
+		client._request.status = 404;
 		return (false);
 	}
 
 	if (access(scriptPath.c_str(), R_OK) != 0) { // R_OK -> readable file
 		Logger::error("CGI script is not readable: " + scriptPath);
+		client._request.err = true;
+		client._request.status = 403;
 		return (false);
 	}
 
