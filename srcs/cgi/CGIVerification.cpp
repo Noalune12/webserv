6849c3281e1587920544d7d3845d2566f294f51f
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2026-02-08 21:27:33 +0100

    chore: removed unecessary lines in CGI verification

--- srcs/cgi/CGIVerification.cpp
+++ srcs/cgi/CGIVerification.cpp
@@ -13,8 +13,6 @@ bool	CGIExecutor::verifyCGIPath(const std::string& path, Connection& client) {
 
 	if (access(path.c_str(), F_OK) != 0) {
 		Logger::error("CGI interpreter not found: " + path);
-		client._request.err = true;
-		client._request.status = 500;
 		client._request.findErrorPage(500, root, client._request._reqLocation->errPage);
 		return (false);
 	}
@@ -23,24 +21,18 @@ bool	CGIExecutor::verifyCGIPath(const std::string& path, Connection& client) {
 
 	if (stat(path.c_str(), &st) != 0) {
 		Logger::error("Cannot stat CGI interpreter: " + path);
-		client._request.err = true;
-		client._request.status = 500;
 		client._request.findErrorPage(500, root, client._request._reqLocation->errPage);
 		return (false);
 	}
 
 	if (!S_ISREG(st.st_mode)) {
 		Logger::error("CGI interpreter is not a regular file: " + path);
-		client._request.err = true;
-		client._request.status = 500;
 		client._request.findErrorPage(500, root, client._request._reqLocation->errPage);
 		return (false);
 	}
 
 	if (access(path.c_str(), X_OK) != 0) {
 		Logger::error("CGI interpreter is not executable: " + path);
-		client._request.err = true;
-		client._request.status = 500;
 		client._request.findErrorPage(500, root, client._request._reqLocation->errPage);
 		return (false);
 	}
@@ -54,8 +46,6 @@ bool	CGIExecutor::verifyCGIScript(const std::string& scriptPath, Connection& cli
 
 	if (access(scriptPath.c_str(), F_OK) != 0) {
 		Logger::error("CGI script not found: " + scriptPath);
-		client._request.err = true;
-		client._request.status = 404;
 		client._request.findErrorPage(404, root, client._request._reqLocation->errPage);
 		return (false);
 	}
@@ -64,24 +54,18 @@ bool	CGIExecutor::verifyCGIScript(const std::string& scriptPath, Connection& cli
 
 	if (stat(scriptPath.c_str(), &st) != 0) {
 		Logger::error("Cannot stat CGI script: " + scriptPath);
-		client._request.err = true;
-		client._request.status = 404;
 		client._request.findErrorPage(404, root, client._request._reqLocation->errPage);
 		return (false);
 	}
 
 	if (!S_ISREG(st.st_mode)) {
 		Logger::error("CGI script is not a regular file: " + scriptPath);
-		client._request.err = true;
-		client._request.status = 404;
 		client._request.findErrorPage(404, root, client._request._reqLocation->errPage);
 		return (false);
 	}
 
 	if (access(scriptPath.c_str(), R_OK) != 0) { // R_OK -> readable file
 		Logger::error("CGI script is not readable: " + scriptPath);
-		client._request.err = true;
-		client._request.status = 403;
 		client._request.findErrorPage(403, root, client._request._reqLocation->errPage);
 		return (false);
 	}
