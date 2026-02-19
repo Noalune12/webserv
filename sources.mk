b2c5e202d36326c821424d201c7030ac0793d08c
gueberso <111804058+AzehLM@users.noreply.github.com>
2026-02-19 13:26:30 +0100

    Remove subdirectory test configurations
    Removed subdirectory test directory and related variables.

--- sources.mk
+++ sources.mk
@@ -1,9 +1,6 @@
 override SRCSDIR	:= srcs/
 override SRCS		= $(addprefix $(SRCSDIR), $(SRC))
 
-override SUBD_TESTDIR	:= subdirectory_test/
-
-
 override CGIDIR			:= cgi/
 override CONFIGDIR		:= config/
 override HTTPDIR		:= http/
@@ -31,14 +28,6 @@ SRC	+= $(addsuffix .cpp, $(MAIN))
 
 override MAIN			:= \
 	main
-
-
-SRC += $(addprefix $(SUBD_TESTDIR), $(addsuffix .cpp, $(SUBD_TESTSRC)))
-
-override SUBD_TESTSRC			:= \
-	# test \
-
-
 
 SRC += $(addprefix $(SERVERDIR), $(addsuffix .cpp, $(SERVERSRC)))
 
